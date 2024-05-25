35b266fdd823bf9a146b47bd97f86db1eae26bbb
Divya Paliwal <div.pali@gmail.com>
2024-05-25 12:04:53 +0530

    first commit

---
+++ src/main/java/com/practise/FAANG/FaangApplication.java
@@ -0,0 +1,13 @@
+package com.practise.FAANG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/FAANG/FaangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.FAANG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
